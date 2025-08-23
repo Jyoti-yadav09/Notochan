6f2142740451386d07316e47c70b8e670fc85b6c
Jyoti-yadav09 <raniyadav09110@gmail.com>
2025-08-23 06:51:13 +0545

    Install lucide-react for icons

--- notochan-frontend/src/App.jsx
+++ notochan-frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 import HomePage from "./pages/HomePage";
 const App=() =>
 {
     return(
-      <>
-       <HomePage/>
-      </>
-    )
+     <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+       
+      </Routes>
+    </Router>
+  );
 }
 
  export default App;
